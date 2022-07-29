5ba8ce9dfaccb5140de3136a951b5ceff736c9f4
ksuzh <ksuzha1@gmail.com>
2022-07-29 19:40:54 +0200

    added method for the main task

--- Program.cs
+++ Program.cs
@@ -19,6 +19,39 @@ string PrintArray(string[] array)
     return showArray;
 }
 
-string[] myArray = CreateAndFillStringArray(5);
 
+
+string[] ResultArray(string[] array, int symbAmount)
+{
+    int CountElements(string[] array)
+    {
+        int count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+            if(array[i].Length <= symbAmount)
+                count++;
+
+        return count;
+    }
+    
+    string[] resultArray = new string[CountElements(array)];
+    int index = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i].Length <= symbAmount)
+        {
+            resultArray[index] = array[i];
+            index += 1;
+        }
+                    
+    }
+    return resultArray;
+}
+
+
+string[] myArray = CreateAndFillStringArray(5);
 Console.WriteLine(PrintArray(myArray));
+
+
+int amount = 3;
+Console.WriteLine(PrintArray(ResultArray(myArray, amount)));
