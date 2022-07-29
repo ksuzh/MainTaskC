031e0a3788a5de6c0c05741af14036356bdac959
ksuzh <ksuzha1@gmail.com>
2022-07-29 18:38:46 +0200

    made method to create and fill array

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿string[] CreateStringAndFillArray(int size)
+{
+    string[] elements = new string[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i +1} word: ");
+        elements[i] = Console.ReadLine();
+    }
+
+    return elements;
+}
+
